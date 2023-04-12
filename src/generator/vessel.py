e42ed4047a8d31394aca352d85e89a8037776009
nazmigorkem <nazmigorkem@hotmail.com>
2023-04-12 20:15:59 +0300

    added vessel type

--- src/generator/vessel.py
+++ src/generator/vessel.py
@@ -3,7 +3,7 @@ import math
 from generator.singleton import Singleton
 from lib.util import Util
 from lib.calculation import Calculation
-POSSIBLE_TYPES = ["A"]
+POSSIBLE_TYPES = [{"type_name":"A", "AIS_range": 10**4, "broadcast_interval": 360}]
 
 class Vessel(metaclass=Singleton):
 
@@ -18,13 +18,12 @@ class Vessel(metaclass=Singleton):
 
     def select_vessel(self, mmsi: int):
         self.selected_vessel = self.vessels_ordered_by_mmsi[mmsi - self.mmsi_starting_number]
-        closest = Util.find_in_range(self.vessels_ordered_by_latitude, self.selected_vessel, 10 ** 4)
+        closest = Util.find_in_range(self.vessels_ordered_by_latitude, self.selected_vessel, self.selected_vessel["type"]["AIS_range"])
    
         return closest
 
 
     def generate(self, coordinates_1: list[float], coordinates_2: list[float], current_route_index, density: int = 5, noise: float = 0.05):
-        
         for _ in range(density):
             current_vessels = []
             rand_point = Calculation.get_random_point(
@@ -38,7 +37,7 @@ class Vessel(metaclass=Singleton):
                 "speed": random.random() * 200,
                 "lon": rand_point[0],
                 "lat": rand_point[1],
-                "type_of_ship": POSSIBLE_TYPES[math.floor(random.random() * len(POSSIBLE_TYPES))],
+                "type": POSSIBLE_TYPES[math.floor(random.random() * len(POSSIBLE_TYPES))],
                 "current_route_index": current_route_index,
                 "last_distance_to_current_mid_point_end": Calculation.calculate_distance(coordinates_2[1], coordinates_2[0], rand_point[1], rand_point[0])
             }
